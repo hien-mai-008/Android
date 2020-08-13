ed2b591dfcf64827adc1ccf031154dcc90cd757c
Ducky919 <ducky_the_legend96@yahoo.com>
2020-08-13 16:12:28 +0900

    Exercise_1 (Layout and View)

---
+++ app/src/main/java/com/hfad/myicon/MainActivity.java
@@ -0,0 +1,20 @@
+package com.hfad.myicon;
+
+import androidx.annotation.RequiresApi;
+import android.app.Activity;
+
+
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
